76a102b685554a45d9d59c3eb88b865db788ea7d
trinh04042000 <letrinh04042000@gmail.com>
2018-11-30 09:11:27 +0700

    add new file

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+
+        $user->name = "adminUser";
+
+        $user->email = "admin@gmail.com";
+
+        $user->password = bcrypt("admin");
+
+        $user->admin = 1;
+
+        $user->save();
+
+        //tao user binh thuong
+
+        $user = new User();
+
+        $user->name = "user";
+
+        $user->email = "user@gmail.com";
+
+        $user->password = bcrypt("user");
+
+        $user->admin = 0;
+
+        $user->save();
+    }
+}
---
+++ resources/views/layouts/page_guest.blade.php
@@ -0,0 +1,5 @@
+<?php
+
+
+echo "xin chao guest";
+
